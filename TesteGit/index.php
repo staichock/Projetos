1e546e6d43ed841374d56e6e8bb7c4ffb358879a
Rodrigo <Rodrigo@Rodrigo-Rein>
2015-11-23 13:31:51 -0200

    Instalação bootstrap

--- TesteGit/index.php
+++ TesteGit/index.php
@@ -1,1 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width">
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="bootstrap/js/bootstrap.js" type="text/javascript"></script>
+
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet"
+	type="text/css" />
+
+<title>Menu Teste</title>
+</head>
+<body>
+
+	<nav class="navbar navbar-default" role="navigation">
+
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse"
+				data-target="#bar1">
+				<span class="sr-only">Toggle navigation</span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+
+			<a class="navbar-brand" href="#">Rein Sistemas</a>
+
+		</div>
+
+		<div class="collapse navbar-collapse" id="bar1">
+			<ul class="nav navbar-nav">
+				<li><a href="#">Instalação Ctrl-e</a></li>
+				<li><a href="#">Instalaçao AllFiscal</a></li>
+				<li><a href="#">Virtual Host</a></li>
+
+			</ul>
+		</div>
+
+	</nav>
+
+</body>
+</html>
